5c8c1e033588fc131fac650a1889509dd6367f33
xxzuo <1293378490@qq.com>
2023-01-10 21:29:10 +0800

    代码调整

--- src/main/java/com/xxzuo/leetcode/editor/cn/CrackingTheSafe.java
+++ src/main/java/com/xxzuo/leetcode/editor/cn/CrackingTheSafe.java
@@ -44,38 +44,39 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class CrackingTheSafe{
-      public static void main(String[] args) {
-           Solution solution = new CrackingTheSafe().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-      // 这题抄题解的 QAQ
-      Set<Integer> seen = new HashSet<Integer>();
-      StringBuffer ans = new StringBuffer();
-      int highest;
-      int k;
+public class CrackingTheSafe {
+    public static void main(String[] args) {
+        Solution solution = new CrackingTheSafe().new Solution();
+    }
 
-      public String crackSafe(int n, int k) {
-          highest = (int) Math.pow(10, n - 1);
-          this.k = k;
-          dfs(0);
-          for (int i = 1; i < n; i++) {
-              ans.append('0');
-          }
-          return ans.toString();
-      }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 这题抄题解的 QAQ
+        Set<Integer> seen = new HashSet<Integer>();
+        StringBuffer ans = new StringBuffer();
+        int highest;
+        int k;
 
-      public void dfs(int node) {
-          for (int x = 0; x < k; ++x) {
-              int nei = node * 10 + x;
-              if (!seen.contains(nei)) {
-                  seen.add(nei);
-                  dfs(nei % highest);
-                  ans.append(x);
-              }
-          }
-      }
-//leetcode submit region end(Prohibit modification and deletion)
+        public String crackSafe(int n, int k) {
+            highest = (int) Math.pow(10, n - 1);
+            this.k = k;
+            dfs(0);
+            for (int i = 1; i < n; i++) {
+                ans.append('0');
+            }
+            return ans.toString();
+        }
 
-  }
+        public void dfs(int node) {
+            for (int x = 0; x < k; ++x) {
+                int nei = node * 10 + x;
+                if (!seen.contains(nei)) {
+                    seen.add(nei);
+                    dfs(nei % highest);
+                    ans.append(x);
+                }
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
