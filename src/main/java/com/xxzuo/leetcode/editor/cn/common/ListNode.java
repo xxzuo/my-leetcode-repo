0ce1406728f1102ddca3afe6240a76a17923fcae
xxzuo <1293378490@qq.com>
2023-01-30 21:19:45 +0800

    1.30每日一题

---
+++ src/main/java/com/xxzuo/leetcode/editor/cn/common/ListNode.java
@@ -0,0 +1,12 @@
+package com.xxzuo.leetcode.editor.cn.common;
+
+/**
+ * 链表
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
