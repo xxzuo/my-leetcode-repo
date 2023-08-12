1dc4c2b39932c86f9e28ee76a767b7f494b44582
xxzuo <1293378490@qq.com>
2023-08-12 14:38:50 +0800

    每日一题

--- src/main/java/com/xxzuo/leetcode/editor/cn/common/ListNode.java
+++ src/main/java/com/xxzuo/leetcode/editor/cn/common/ListNode.java
@@ -6,7 +6,7 @@ package com.xxzuo.leetcode.editor.cn.common;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
